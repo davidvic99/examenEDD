c4e15219018a19c55cfec3c6c41ce7a89126f1f6
DAVID VICTORIA MARTINEZ <davima@floridauniversitaria.es>
2019-05-10 11:48:21 +0200

    Registro y conexion

--- registo.php
+++ registo.php
@@ -1,10 +1,20 @@
 <?php
 
-require_once('registro.php');
+require_once('conexion.php');
 
 if(!empty($_POST['usuario'])&&(!empty($_POST['email']))&&(!empty($_POST['password'])&&(!empty($_POST['nombre'])))){
 
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $nombre = $_POST['nombre'];
+
+    $password = sha1($password);
+
     
+    mysqli_query($conn,"INSERT INTO `usuario` (`id`, `nombre`, `usuario`, `email`, `password`, `fechaRegistro`) VALUES (NULL, '$nombre', '$usuario', '$email', '$password', CURRENT_TIMESTAMP)");
 
+}else{
 
+    echo "Algun campo esta vacio";
 }
