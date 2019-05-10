94a43a837ea8c5634d72db8873a5e387f8b886c8
vicente98fg <43605917+vicente98fg@users.noreply.github.com>
2019-05-10 12:00:34 +0200

    inicio del login

---
+++ inicio.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<h2>Login</h2>
+<form class="" action="login.php" method="post">
+  <div class="grupoFormItem">
+    <label for="usuario"></label>
+      <span class="formLabel">Usuario </span>
+      <input type="text" name="usuario" value="">
+  </div>
+  <div class="grupoFormItem">
+    <label for="contra"></label>
+      <span class="formLabel">Contraseña </span>
+      <input type="text" name="contra" value="">
+  </div>
+  
+</body>
+</html>
