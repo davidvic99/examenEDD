bee508d29b23e1eddc785502ab7b6cd0da537a12
ALUMNADO\rovipo <vinesroberto13@gmail.com>
2019-05-10 11:52:47 +0200

    Añado el login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="registro.php" method="post">
+      <h2>LOGIN</h2>
+      <form  action="registro.php" method="post">
+        Usuario<br>
+        <input type="text" name="usuario" id="usuario" value=""><br>
+        <br>Contraseña<br>
+        <input type="text" name="contraseña" id="contraseña" value="">
+        <br><br>
+        <input type="submit" name="" value="LOGIN" onclick="return comprobar();">
+      </form>
+    </form>
+  </body>
+</html>
